ba2090d0fd0897c50d4e1f2db35bb8848d2f52bf
digis <digis@192.167.0.68>
2026-01-20 11:50:55 -0600

    Inicializacion de proyecto de BancoCliente

---
+++ src/main/java/com/Banco/CajerosCliente/CajerosClienteApplication.java
@@ -0,0 +1,13 @@
+package com.Banco.CajerosCliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CajerosClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CajerosClienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Banco/CajerosCliente/CajerosClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Banco.CajerosCliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CajerosClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
